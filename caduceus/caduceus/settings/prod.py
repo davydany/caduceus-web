84b1c037506022bebdc7f2fded32cf9bfb5983a2
David Daniel <davydany@gmail.com>
2017-06-26 00:05:58 -0400

    trying something for template dir

--- caduceus/caduceus/settings/prod.py
+++ caduceus/caduceus/settings/prod.py
@@ -6,3 +6,21 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 DEBUG = True
+
+TEMPLATES[0]['DIRS'] = os.path.join(BASE_DIR)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(os.path.dirname(BASE_DIR), 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
